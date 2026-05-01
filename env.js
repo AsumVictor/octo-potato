96c121f5101a371f453ea9f9e2259c54e10f2753
Victor Asum <iamasum369@gmail.com>
2026-05-01 14:37:55 +0000

    Make Official for testers

---
+++ env.js
@@ -0,0 +1,4 @@
+window.ENV = {
+  SUPABASE_URL:  'https://towpvlxkzqopdelycfbb.supabase.co',
+  SUPABASE_ANON: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRvd3B2bHhrenFvcGRlbHljZmJiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzc1OTE3NTIsImV4cCI6MjA5MzE2Nzc1Mn0.n7MK_jA8T6BFKLOnU0SbxynVoinDYy0s3LXhIFmeBww'
+};
